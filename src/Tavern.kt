0cd151cd7ac34bf8bc69946c865e1f7da19c651e
remek <r.m.l.marciniak@gmail.com>
2020-07-01 01:01:38 +0200

    8.0

--- src/Tavern.kt
+++ src/Tavern.kt
@@ -1,18 +1,42 @@
+import kotlin.math.roundToInt
+
 const val TAVERN_NAME = "Szynk Hipolit'a"
+const val GOLD_TO_DRAGON_COIN_RATE = 1.43
+var playerDragonCoins = 5.0
+var playerGold = (playerDragonCoins * GOLD_TO_DRAGON_COIN_RATE).toInt()
+var playerSilver = ((playerDragonCoins * GOLD_TO_DRAGON_COIN_RATE) % 1 * 100).roundToInt()
+var dragonBreathBarrels = 5.0
 fun main(args : Array<String>) {
+    println(playerGold)
+    println(playerSilver)
 //    placeOrder()
     placeOrder("shandy,Dragon Breath, 5.91")
-    placeOrder("elixir,Pidgeon, 4.83")
+    placeOrder("shandy,Dragon Breath, 5.91")
+    placeOrder("shandy,Dragon Breath, 5.91")
+    placeOrder("shandy,Dragon Breath, 5.91")
+    placeOrder("shandy,Dragon Breath, 5.91")
+    placeOrder("shandy,Dragon Breath, 5.91")
+    placeOrder("shandy,Dragon Breath, 5.91")
+    placeOrder("shandy,Dragon Breath, 5.91")
+    placeOrder("shandy,Dragon Breath, 5.91")
+    placeOrder("shandy,Dragon Breath, 5.91")
+    placeOrder("shandy,Dragon Breath, 5.91")
+    placeOrder("shandy,Dragon Breath, 5.91")
+    placeOrder("shandy,Dragon Breath, 5.91")
+//    placeOrder("elixir,Pidgeon, 4.83")
 
-    val capitalA :Char = 'A'
-    println(capitalA)
-    val unicodeCapitalA : Char = '\u0041'
-    println(unicodeCapitalA)
-    val omSymbol = '\u0950'
-    println(omSymbol)
-    "Dragon Breath".forEach {
-        println("$it\n")
-    }
+//    val omSymbol = '\u0950'
+//    println(omSymbol)
+//    println(Integer.toBinaryString(42))
+//    println(Integer.toBinaryString(-42))
+//    println(42.shl(1))
+//    println(42.shr(1))
+//    println(42.inv())
+//    println(42.xor(33))
+//    println(42.or(33))
+//    println(42.and(33))
+
+
 }
 private fun placeOrder(){
     val indexOfApostrophe = TAVERN_NAME.indexOf('\'')
@@ -22,29 +46,81 @@ private fun placeOrder(){
 private fun placeOrder(menuData : String){
     placeOrder()
 
-//    val data = menuData.split(',')
-//    val type = data[0]
-//    val name = data[1]
-//    val price = data[2]
-//    destructurization
     val (type, name, price) = menuData.split(',')
 
-    val message = "Mordowycz has bought $name ($type) for $price"
-    println(message)
 
-//    val phrase = "Wow... this $name is amazing!"
-//    print("Mordowycz: ${toDragonSpeak(phrase)}")
+    val isEnoughMoney = performPurchase(price.toDouble())
 
-    val phrase = if(name == "Dragon Breath"){
-        "Mordowycz: ${toDragonSpeak("Wow... this $name is amazing! AAAAAAAAA")}"
+    if(isEnoughMoney){
+        val message = "Mordowycz has bought $name ($type) for $price"
+        println(message)
+
+//    val gold : Int = "5.91".toIntOrNull() ?: 0
+
+
+        val phrase = if(name == "Dragon Breath"){
+            "Mordowycz: ${toDragonSpeak("Wow... this $name is amazing! AAAAAAAAA")}"
+        }else{
+            "Mordowycz: thank you for $name"
+        }
+        println(phrase)
     }else{
-        "Mordowycz: thank you for $name"
+        println("Mordowycz doesn't have enough money")
     }
-    println(phrase)
+
+}
+private fun performPurchase(price : Double) : Boolean{
+    displayBalance()
+    val totalPurse = playerGold + playerSilver/100.0
+//    println("Total money: $totalPurse")
+    println("Total money: ${"%.4f".format(playerDragonCoins)}")
+    println("Ordering a product for $price")
+    val remainingBalance = totalPurse - price
+    return if(remainingBalance < 0){
+        false
+    }else {
+//        println("Remaining total money: ${"%.2f".format(remainingBalance)}")
+        val remainingDragonCoins = remainingBalance / GOLD_TO_DRAGON_COIN_RATE
+        println("Remaining total money: ${"%.4f".format(remainingDragonCoins)}")
+        playerDragonCoins = remainingDragonCoins
+        playerGold = (playerDragonCoins * GOLD_TO_DRAGON_COIN_RATE).toInt()
+        playerSilver = ((playerDragonCoins * GOLD_TO_DRAGON_COIN_RATE) % 1 * 100).roundToInt()
+        println(playerGold)
+        println(playerSilver)
+
+//        val remainingGold = remainingBalance.toInt()
+//        val remainingSilver = (remainingBalance % 1 * 100).roundToInt()
+//        playerGold = remainingGold
+//        playerSilver = remainingSilver
+        displayBalance()
+
+        dragonBreathBarrels -= 0.125
+        displayDragonBreathStock()
+        true
+    }
+}
+
+
+
+private fun displayDragonBreathStock(){
+    println("Remaining Dragon Breath barrels: $dragonBreathBarrels")
+//    if(( (dragonBreathBarrels - 2.0) % 1.5) == 0.0){
+//        val remainingDragonBreathGlasses = ( dragonBreathBarrels / 0.125 ).toInt()
+//        println("Remaining Dragon Breath glasses: $remainingDragonBreathGlasses")
+//    }
+    if( dragonBreathBarrels == 3.5){
+        val remainingDragonBreathGlasses = ( dragonBreathBarrels / 0.125 ).toInt()
+        println("Remaining Dragon Breath glasses: $remainingDragonBreathGlasses")
+    }
+}
+
+private fun displayBalance(){
+//    println("Player sack: gold $playerGold , silver $playerSilver")
+    println("Player sack: dragon coins ${"%.4f".format(playerDragonCoins)}")
 }
 
 private fun toDragonSpeak(phrase : String) : String {
-    return phrase.replace(Regex("[aeiouAEIOU]"),{
+    return phrase.replace(Regex("[aeiouAEIOU]")) {
         when (it.value){
             "A" -> "4"
             "E" -> "3"
@@ -58,7 +134,7 @@ private fun toDragonSpeak(phrase : String) : String {
             "u" -> "|_|"
             else -> it.value
         }
-    })
+    }
 }
 
 
